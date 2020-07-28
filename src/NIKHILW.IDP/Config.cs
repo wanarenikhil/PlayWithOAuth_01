17c678c52d432b1d8247c5b1bbd6b5ac2dfb7401
wanarenikhil <nikhilwanare@gmail.com>
2020-07-28 08:26:40 +0530

    Add identity server

---
+++ src/NIKHILW.IDP/Config.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace NIKHILW.IDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client{
+                     ClientName="Image Gallery",
+                     ClientId ="imagegalleryclient",
+                     AllowedGrantTypes = GrantTypes.Code,
+                     RedirectUris = new List<string>()
+                     {
+                         "https://localhost:5001/signin-oidc"
+                     },
+                     AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
+    }
+}
---
+++ src/NIKHILW.IDP/Quickstart/TestUsers.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "818727",
+                        Username = "Frank",
+                        Password = "password",
+                        Claims =
+                        {
+                            new Claim("given_name", "Frank"),
+                            new Claim("family_name", "Underwood")
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "88421113",
+                        Username = "Claire",
+                        Password = "password",
+                        Claims =
+                        {
+                            new Claim("given_name", "Claire"),
+                            new Claim("family_name", "Underwood")
+                        }
+                    }
+                };
+            }
+        }
+    }
+}   
